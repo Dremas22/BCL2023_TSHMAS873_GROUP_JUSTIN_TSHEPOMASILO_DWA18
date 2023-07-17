6eb6a609aff972f1cdd5a8f188058c8fe6ac8acb
Tshepo Masilo <76808634+Dremas22@users.noreply.github.com>
2023-07-17 14:59:46 +0200

    saving changes

---
+++ podcast-app/src/components/nav.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+export default function NavBar() {
+
+    const [showNavContainer, setShowNavContainer] = React.useState(false);
+
+    const handleMouseLeave = () => {
+        setShowNavContainer(false);
+    };
+
+    const handleMouseEnter = () => {
+        setShowNavContainer(true);
+    };
+
+    return (
+
+        <nav className="nav-cast" onMouseLeave={handleMouseLeave}>
+            <div className="title-btn" onMouseEnter={handleMouseEnter}>
+                <h3>Try me!</h3>
+                <img src="./images/microphone.png" className="mic" alt="mic" />
+            </div>
+            {showNavContainer && (
+                <div className="navContainer">
+                    <button className="btn-nav">Home</button>
+                    <button className="btn-nav">News</button>
+                    <button className="btn-nav">Business</button>
+                    <button className="btn-nav">History</button>
+                    <button className="btn-nav">Comedy</button>
+                    <button className="btn-nav">Kids & Family</button>
+                    <button className="btn-nav">Fiction</button>
+                </div>
+            )}
+
+        </nav>
+    )
+}
+
